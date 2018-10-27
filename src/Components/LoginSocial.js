8501e7591891f0cca478267bd29ca822d8b64a6d
Omar <omarelnagdy16@gmail.com>
2018-10-27 18:51:38 -0400

    Social Media login page

---
+++ src/Components/LoginSocial.js
@@ -0,0 +1,64 @@
+import "./App.css"
+import firebase from "firebase"
+import React, {Component} from "react
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+import Header from './Components/Header';
+import Signin from './Components/LoginPage/SignIn';
+
+firebase.initializeApp({
+  apiKey : "AIzaSyDVL3Lvrvc0E4i6CiuY3Ay9wkjvZona4y0",
+  authDomain : "login-form-try-2.firebaseapp.com",
+})
+
+class App extends Component {
+  state = {isSignedIn : false} uiConfig = {
+    signInFlow : "popup",
+    signInOptions:
+        [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+    callbacks: {signInSuccess: () => false}
+  }
+
+  componentDidMount =
+      () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+      }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to AMustMovies </h1>
+        {this.state.isSignedIn ? (
+          <span>
+            {/* <Header /> */}
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+              <Signin />
+            {/* <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            /> */}
+          </span>
+        ) : (
+
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+            style={{position:'center'}}
+          />
+        )
+  }
+  < /div>
+    )
+  }
+}
+
+export default App;
