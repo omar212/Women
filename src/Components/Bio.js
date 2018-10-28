fcbb92c9742fffdc0d6578eb68a674e7ceb171c2
Andy Wang <andywang219@yahoo.com>
2018-10-28 01:03:18 -0400

    Extracted info from a json file and displayed it

--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -1,19 +1,84 @@
 import React, {Component} from "react";
 import Template from "./Template";
-import * as data from "./data.json";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Data from "./data.json";
+
 
 class Bio extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: data.Name,
+            filter: "",
+            key: [],
+            results: 0,
         }
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange = filter => event => {
+        this.setState({
+            filter: event.target.value,
+        });
+    };
+
     render() {
-        console.log(data.name);
-        return(
+        const career = [{
+                value: '',
+                label: '',
+            },
+            {
+                value: 'Mathematics',
+                label: 'Mathematics',
+            },
+            {
+                value: 'Computer Science',
+                label: 'Computer Science',
+            },
+            {
+                value: 'Computer Engineering',
+                label: 'Computer Engineering',
+            },
+            {
+                value: 'Biochemistry',
+                label: 'Biochemistry',
+            }
+        ];
+
+        return (
             <div>
-                <h1>{this.state.name}</h1>
+                <TextField 
+                    select
+                    label = "Career"
+                    value = {this.state.filter}
+                    variant = "outlined"
+                    style = {{width: "200px", borderColor: "red"}}
+                    onChange = {this.handleChange('career')}
+                >
+                    {career.map(option => (
+                        <MenuItem key = {option.value} value = {option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <div>
+                    {Data.map((data, index) => {
+                        // if (data.Keyword === this.state.filter) {
+                           return (
+                               <div>                       
+                                    <Template 
+                                        name = {data.Name} 
+                                        born = {data.Year}
+                                        career = {data.Keyword}
+                                        info = {data.Description} 
+                                        image = {data.Picture} 
+                                        moreInfo = {data.Wiki}
+                                    />
+                                </div>
+                           )
+                        // }
+                    })}
+                </div>
             </div>
         )
     }
--- src/Components/Template.js
+++ src/Components/Template.js
@@ -4,6 +4,16 @@ import Typography from "@material-ui/core/Typography";
 import "./Andy.css";
 
 class Template extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.name,
+            year: this.props.born,
+            info: this.props.info,
+            image: this.props.image,
+            moreInfo: this.props.moreInfo,
+        }
+    }
     render() {
         return (
             <div>
@@ -14,41 +24,28 @@ class Template extends Component {
                     data-aos-easing = "linear"
                     data-aos-duration = "500"
                 >   
-                    <img src = {require("./pete-bellis-199853-unsplash.jpg")} alt = ""></img>
+                    < img src = {this.state.image} alt = ""></img>
 
                     <Typography
                         variant = "display1"
                         style = {{color: "black"}}
                     >
-                        Women's Name
+                        {this.state.name}
                     </Typography>
 
                     <Typography
                         variant = "subtitle1"
                     >
-                        (1887 - 1945) 
-                    </Typography>
-
-                    <Typography
-                        variant = "subtitle1"
-                    >
-                        Age:
+                        {this.state.year} 
                     </Typography>
 
                     <Typography
                         variant = "body2"
                     >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget maximus nibh. Aenean bibendum ligula quis
-                        augueiaculis posuere sit amet eget nulla. Mauris vel tellus nibh. Maecenas eget interdum ipsum, vel vestibulum
-                        eros. Integer sit amet velit sed risus ultricies lacinia. Donec a magna felis. Curabitur eu nisi feugiat,
-                        tincidunt eros quis, aliquet ligula.
+                        {this.state.info}
                     </Typography> 
-                    
-                    <Typography
-                        variant = "subtitle1"
-                    >
-                        Relevant Info:
-                    </Typography>
+                
+                    < a href = {this.state.moreInfo} >More Info</a>
                 </Card>
             </div>
         )
